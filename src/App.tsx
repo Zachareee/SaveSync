cc2c785d93296c6fcec8849ada0ff1c5f5f2de15
Zachareee <zacharytanhx@yahoo.com>
2025-01-31 12:47:17 +0800

    Swap to SolidJS

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import { useEffect, useState } from "react";
 import { invoke } from "./utils.ts";
 import "./App.css";
 import { Info } from "./types.ts";
+import { createSignal, For } from "solid-js";
+
 function App() {
-  const [services, setServices] = useState<Info[]>([]);
-  useEffect(() => {
-    invoke("get_plugins").then(setServices);
-  }, []);
+  const [services, setServices] = createSignal<Info[]>([]);
+
+  invoke("get_plugins").then(setServices);
+
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React + Lua</h1>
-      {services.map(({ name, description, author }, idx) => (
-        <div key={idx}>
-          <h2>{name}</h2>
-          <p>Description: {description}</p>
-          <span>Written by: {author}</span>
-        </div>
-      ))}
+    <main class="container">
+      <h1>Welcome to Tauri + Solid + Lua</h1>
+      <For each={services()}>
+        {({ name, description, author }) => (
+          <div>
+            <h2>{name}</h2>
+            <p>Description: {description}</p>
+            <span>Written by: {author}</span>
+          </div>
+        )}
+      </For>
     </main>
   );
 }
+
 export default App;
--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,12 @@
 import { invoke as old_invoke } from "@tauri-apps/api/core";
 import { Info } from "./types.ts";
+
 export function invoke<T extends keyof InvokeTypes>(
   command: T,
 ): Promise<InvokeTypes[T]> {
   return old_invoke(command);
 }
+
 type InvokeTypes = {
   get_plugins: Info[];
 };
