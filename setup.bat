92b73920d185a83f8040c89018ded1b58f360ce6
Zachareee <zacharytanhx@yahoo.com>
2025-03-19 17:17:35 +0800

    Update setup bat file

--- setup.bat
+++ setup.bat
@@ -1,1 +1,2 @@
-curl -L https://sourceforge.net/projects/luabinaries/files/5.4.2/Windows%20Libraries/Dynamic/lua-5.4.2_Win64_dll17_lib.zip/download --output - 2> nul | tar t
+curl -Ls https://sourceforge.net/projects/luabinaries/files/5.4.2/Windows%%20Libraries/Dynamic/lua-5.4.2_Win64_dll17_lib.zip/download -o lua.zip
+tar xf lua.zip -C src-tauri lua54.dll
